a659686fff9ccf0f2be4cee7cb3a5d754c155357
nico <varela.nicoo@gmail.com>
2023-02-06 16:53:48 -0300

    modal

---
+++ js/modal.js
@@ -0,0 +1,13 @@
+const openModal = document.querySelector(".btn-modal")
+const modal = document.querySelector(".modal")
+const closeModal = document.querySelector(".modal-close")
+
+openModal.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.classList.add("modal-show")
+})
+
+closeModal.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.classList.remove("modal-show")
+})
